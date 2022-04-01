70f08428986410f2c13a1346934edfe325ac8147
dibss <ludovicad01@gmail.com>
2022-04-01 15:36:38 +0200

    .

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,5 @@
-// Ogni todo sarà un oggetto, formato da due proprietà:
-// - text, una stringa che indica il testo del todo
-// - done, un booleano (true/false) che indica se il todo è stato fatto oppure no
-// MILESTONE 1
-// Stampare all'interno di una lista, un item per ogni todo.
-// Se la proprietà done è uguale a true, visualizzare il testo del todo sbarrato.
-// MILESTONE 2
-// Visualizzare a fianco ad ogni item ha una "x": cliccando su di essa, il todo viene rimosso dalla lista.
-// MILESTONE 3
-// Predisporre un campo di input testuale e un pulsante "aggiungi": cliccando sul pulsante, il testo digitato viene letto e utilizzato per creare un nuovo todo, che quindi viene aggiunto alla lista dei todo esistenti.
-// Bonus:
-// 1- oltre al click sul pulsante, intercettare anche il tasto ENTER per aggiungere il todo alla lista
-// 2- cliccando sul testo dell'item, invertire il valore della proprietà done del todo corrispondente (se done era uguale a false, impostare true e viceversa)
 // BONUS extra:
 // non permettere l'inserimento di todo doppie nell'array di oggetti
-// Buon lavoro e buon divertimento!
 
 var app = new Vue({
   el : "#root",
@@ -35,12 +21,26 @@ var app = new Vue({
   },
 
   methods : {
-    taskDone: function(){
-      this.todo.done = true;
+    add: function(){
+      let task = document.getElementById("newTodo").value,
+      newTodo = {
+        text : task,
+        done : false
+      }
+      // DA SISTEMARE SE L'ELEMENTO E' GIA STATO INSERITO
+      if(this.todo.includes(task)){
+        alert("Il task è già stato inserito");
+      } else if(task == ""){
+        alert("Non hai inserito nessun task!")
+      } else {
+        this.todo.push(newTodo);
+      }
     },
-    
-    add : function(){
-
-    }
+    deleteTask: function(i){
+      this.todo.splice(i, 1)
+    },
+    taskDone: function(i){
+      this.todo[i].done = true;
+    },
   },
 });
